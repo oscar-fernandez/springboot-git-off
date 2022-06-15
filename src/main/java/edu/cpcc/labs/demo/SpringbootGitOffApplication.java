8914f0189da6a31d12e05fa87255e7c486ddf11c
oscar-fernandez <68928859+oscar-fernandez@users.noreply.github.com>
2022-06-15 14:30:25 -0400

    Initial version.

---
+++ src/main/java/edu/cpcc/labs/demo/SpringbootGitOffApplication.java
@@ -0,0 +1,13 @@
+package edu.cpcc.labs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitOffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGitOffApplication.class, args);
+	}
+
+}
